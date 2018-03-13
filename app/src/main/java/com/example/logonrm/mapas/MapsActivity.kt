4165193473be17776240bde7eca609332ebc8e77
rm330072 <carlosrpcaldas@hotmail.com>
2018-03-12 22:44:49 -0300

    Versao Funcionando antes do: https://javapapers.com/android/android-location-fused-provider/

--- app/src/main/java/com/example/logonrm/mapas/MapsActivity.kt
+++ app/src/main/java/com/example/logonrm/mapas/MapsActivity.kt
@@ -1,9 +1,23 @@
 package com.example.logonrm.mapas
 
+import android.Manifest
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.FragmentActivity
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,9 +29,110 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
+        //LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+/*    override fun onLocationChanged(location: Location) {
+        Log.d("TAG", "Firing onLocationChanged..............................................")
+        mCurrentLocation = location
+        mLastUpdateTime = DateFormat.getTimeInstance().format(Date())
+        updateUI()
+    }
+
+*/
+
+    val REQUEST_GPS = 212
+
+    override fun onConnected(p0: Bundle?) {
+
+        checkPermission()
+
+        val minhaLocallizacao = LocationServices
+                .FusedLocationApi
+                .getLastLocation(mGoogleApiClient)
+
+
+        if (minhaLocallizacao != null)
+        adicionarMarcador(minhaLocallizacao.latitude,
+                minhaLocallizacao.longitude,
+                "Nos estamos aqui!!!")
+
+    }
+
+    private fun checkPermission() {
+        val permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i("", "Permissão para gravar negada")
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                val builder = AlertDialog.Builder(this)
+
+                builder.setMessage("Necessária a permissao para GPS")
+                        .setTitle("Permissao Requerida")
+
+                builder.setPositiveButton("OK") { dialog, id ->
+                    Log.i("TAG", "Clicked")
+                    requestPermission()
+                }
+
+                val dialog = builder.create()
+                dialog.show()
+
+            } else {
+                requestPermission()
+            }
+        }
+    }
+
+    protected fun requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_GPS)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_GPS -> {
+                if (grantResults.size == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i("TAG", "Permissão negada pelo usuário")
+
+                } else {
+                    Log.i("TAG", "Permissao concedida pelo usuario")
+                }
+                return
+            }
+        }
+    }
+
+
+    override fun onConnectionSuspended(p0: Int) {
+        Log.i("TAG", "SUSPENSO")
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Log.i("TAG", "Erro de conexao")
+    }
 
     private lateinit var mMap: GoogleMap
+    private lateinit var mGoogleApiClient : GoogleApiClient
+
+    @Synchronized fun callConnection(){
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addOnConnectionFailedListener { this }
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build()
+
+        mGoogleApiClient.connect()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,15 +143,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         btPesquisar.setOnClickListener {
+
+            mMap.clear()
+
             val geocoder = Geocoder(this)//Geocoder( context: this)
             var address: List<Address>?
 
             address = geocoder.getFromLocationName(etEndereco.text.toString(),
                     1)
-            val location = address[0]
 
-            adicionarMarcador(location.latitude, location.longitude, "Endereco pesquisado")
+            if(address.isNotEmpty()) {
+                val location = address[0]
+                adicionarMarcador(location.latitude, location.longitude, "Endereco pesquisado")
 
+            }else {
+                var alert = AlertDialog.Builder(this).create()
+                alert.setTitle("Erro!!!")
+                alert.setMessage("Endereco nao existe")
+
+                alert.setCancelable(true)
+                alert.setButton(AlertDialog.BUTTON_POSITIVE, "OK", {
+                    dialogInterface, inteiro ->
+                    alert.dismiss()
+                })
+
+                alert.show()
+
+                // Toast.makeText(this,"Endereco nao exite", Toast.LENGTH_LONG)
+
+            }
         }
 
     }
@@ -64,9 +199,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        callConnection()
+
+
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-23.564202, -46.652671)
+    /*    val sydney = LatLng(-23.564202, -46.652671)
         mMap.addMarker(MarkerOptions()
                 .position(sydney)
                 .title("Marker in FIAP")
@@ -75,5 +213,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
 
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16f))
+    */
     }
+
 }
